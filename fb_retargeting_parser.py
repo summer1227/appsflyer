92eab818c2ed1063ea32e460316c4cbd7d4ac1d2
summer1227 <wwang04@gmail.com>
2016-08-30 17:54:51 +0800

    Updated to output a file
    Update the script to output those matching the criteria into an output
    file

--- fb_retargeting_parser.py
+++ fb_retargeting_parser.py
@@ -6,7 +6,7 @@
 import os
 import pandas as pd
 
-def parse_file(path):
+def parse_file(path, out):
 	print ("Parsing ", path)
 	# make sure null bytes are replaced. Otherwise will throw exception when processing
 	fi = open(path, 'r', encoding='utf-8')
@@ -16,24 +16,35 @@ def parse_file(path):
 	fo.write(data.replace('\x00', ''))
 	fo.close()
 	
-	fb = pd.read_csv(path+'.tmp', header=3, names=['req', 'resp'],usecols=[0,1], delimiter='\t')
+	fb = pd.read_csv(path+'.tmp', names=['req', 'resp', 'payload'], delimiter='\t')
+	fb = fb.dropna()
 	counter = 0
 	# read second column, and remove NaN and null
-	response = [x for x in list(fb['resp'].dropna()) if x != 'null']
-	for r in response:
-		if "\"is_fb\":true" in r:
+	# response = [x for x in list(fb['resp'].dropna()) if x != 'null']
+	
+	for index, row in fb.iterrows():
+		if "\"is_fb\":true" in row['resp']:
 			counter += 1
-
+			out.write(str(counter) + ":\n")
+			out.write("Request: " + row['req'] + "\n")
+			out.write("Response: " + row['resp'] + "\n")
+			out.write("Payload: " + row['payload'] + "\n")
+			out.write("\n")
 	return counter
 	
 
 filepaths = os.listdir('.') # all files in the current directory
 total = 0
+out = open("out.txt", 'w', encoding='utf-8')
+
 for filepath in filepaths:
     if filepath.endswith('.tsv'):
         path = filepath
-        total += parse_file(path)
-		
+        total += parse_file(path, out)
+
+
+out.close()
+
 print(total)
 
 
